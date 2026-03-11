8a7361fb73a7477a435678b8bcc34fde3db9478f
Zach Seitz <zseitz@uw.edu>
2026-03-11 16:48:16 -0700

    Added smoke test

--- tests/test_datanavi.py
+++ tests/test_datanavi.py
@@ -2,5 +2,10 @@ import pytest
 
 import nanoporethon
 
+import os
+
+def test_smokeTestSubcomponent4():
+	os.system("python subcomponent_4_data_navi_gui.py")
+
 def test_mytest():
 	print("test ran")
